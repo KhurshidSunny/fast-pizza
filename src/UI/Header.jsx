a78c2f4e4d849dca83b11f5cba86424009a73c41
Khurshid Khan <khurshidsunny45@gmail.com>
2023-08-25 09:36:41 -0700

    Setting up the custom Font Family

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -4,7 +4,7 @@ import Username from './Username';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 sm:px-6">
+    <header className=" flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 sm:px-6">
       <Link to="/" className="uppercase tracking-widest">
         Fast Pizza Co.
       </Link>
