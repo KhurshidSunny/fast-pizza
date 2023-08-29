5a99fc61bc6d4fb2ec6d93d2cf10a89bd3d012ed
Khurshid Khan <khurshidsunny45@gmail.com>
2023-08-28 23:44:12 -0700

    Fetching Ingredients data in the Order component without Navigating but using useFetcher hook and showing this ingredients data in the order items

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -6,8 +6,9 @@ import {
   formatDate,
 } from '../../utilities/helpers';
 import { getOrder } from '../../services/apiRestaurant';
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import OrderItem from './OrderItem';
+import { useEffect } from 'react';
 function Order() {
   const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
@@ -22,7 +23,15 @@ function Order() {
   } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
-  console.log(id, cart);
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
   return (
     <div className="space-y-8 px-4 py-6">
       <div className="flex flex-wrap items-center justify-between gap-2">
@@ -53,7 +62,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-300 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId).ingredients ??
+              []
+            }
+          />
         ))}
       </ul>
 
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -2,17 +2,20 @@
 import { formatCurrency } from '../../utilities/helpers';
 
 // function OrderItem({ item, isLoadingIngredients, ingredients }) {
-function OrderItem({ item }) {
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="space space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? 'Loading...' : ingredients.join(', ')}
+      </p>
     </li>
   );
 }
